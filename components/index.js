c5beb232c34c90913336d0aee58eb937345bd5f9
khoirulkahf <fahrulista@gmail.com>
2022-08-28 12:29:05 +0700

    ui/ux done

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+export {
+  DetailsDesc,
+  DetailsBid,
+  CircleButton,
+  RectButton,
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  SubInfo,
+};
